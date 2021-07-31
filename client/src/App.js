67c78af5f3fb70d7b42780c9e08c86ca36132434
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-30 21:15:26 -0400

    Alex files here

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+import GoogleMap from "./components/GoogleMap/GoogleMaps";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/Signup";
+import Ratings from "./components/Ratings/Ratings";
+import Marker from "./components/Marker/Marker";
+import Nav from "./components/Nav/Nav";
+import Restrooms from "./components/Restrooms/Restrooms";
+//import Home from './components/Home/Home'
+//import Home from './pages/Home';
+//import Maps from './components/Maps';
+//import Detail from './pages/Detail';
+//import NoMatch from './pages/NoMatch';
+//import Login from './components/Login/Login';
+//import Signup from './pages/Signup';
+//import Nav from './components/Nav';
+//import { StoreProvider } from './utils/GlobalState';
+//import LandingPage from './componets/LandingPAge';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -41,20 +49,16 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </StoreProvider>
-        </div>
+        <Switch>
+          
+          <Route exact path="/" component={GoogleMap} />
+          <Route exact path="/Restrooms" component={Restrooms} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Marker" component={Marker} />
+          <Route exact path="/Nav" component={Nav} />
+          <Route exact path="/Ratings" component={Ratings} />
+          <Route exact path="/SignUp" component={SignUp} />
+        </Switch>
       </Router>
     </ApolloProvider>
   );
